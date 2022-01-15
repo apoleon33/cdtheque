0be0a45b1b6315ba2687b8960d024be6cc8b312f
apoleon33 <simondumesny@gmail.com>
2022-01-15 16:27:06 +0100

    improvement in responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(const Myapp());
 }
 
-class Myapp extends StatelessWidget {
+class Myapp extends StatefulWidget {
   const Myapp({Key? key}) : super(key: key);
 
   @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            backgroundColor: const Color(0xff222831),
-            body: Container(
-              child: Row(
+    return MaterialApp(home: Cd());
+  }
+}
+
+//create new class for "home" property of MaterialApp()
+class Cd extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xff222831),
+        body: Container(
+          child: Column(
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Center(
                         child: Container(
-                      child: Image.asset('cd_example.png'),
+                      child: Image.asset(
+                        'cd_example.png',
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.4,
+                      ),
                       margin: const EdgeInsets.all(10),
                       padding: const EdgeInsets.all(10),
 
-                      height: 300,
-                      width: 300,
-                      //BoxShadow(color: Colors.green, spreadRadius: 3)
-                      decoration: BoxDecoration(
-                          color: const Color(0xff393e46),
-                          borderRadius: BorderRadius.circular(15)),
-                    )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                        child: Container(
-                      child: Image.asset('cd_example.png'),
-                      margin: const EdgeInsets.all(10),
-                      padding: const EdgeInsets.all(10),
-
-                      height: 300,
-                      width: 300,
-                      //BoxShadow(color: Colors.green, spreadRadius: 3)
-                      decoration: BoxDecoration(
-                          color: const Color(0xff393e46),
-                          borderRadius: BorderRadius.circular(15)),
-                    )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                        child: Container(
-                      child: Image.asset('cd_example.png'),
-                      margin: const EdgeInsets.all(10),
-                      padding: const EdgeInsets.all(10),
-
-                      height: 300,
-                      width: 300,
-                      //BoxShadow(color: Colors.green, spreadRadius: 3)
-                      decoration: BoxDecoration(
-                          color: const Color(0xff393e46),
-                          borderRadius: BorderRadius.circular(15)),
-                    )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                        child: Container(
-                      child: Image.asset('cd_example.png'),
-                      margin: const EdgeInsets.all(10),
-                      padding: const EdgeInsets.all(10),
-
-                      height: 300,
-                      width: 300,
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.4,
                       //BoxShadow(color: Colors.green, spreadRadius: 3)
                       decoration: BoxDecoration(
                           color: const Color(0xff393e46),
@@ -83,6 +53,8 @@ class Myapp extends StatelessWidget {
                   ),
                 ],
               ),
-            )));
+            ],
+          ),
+        ));
   }
 }
