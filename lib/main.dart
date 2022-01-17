43eaaf496933e86214f6ade916c4387210e06025
apoleon33 <simondumesny@gmail.com>
2022-01-17 19:57:17 +0100

    working widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,46 @@ class Myapp extends StatefulWidget {
 class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Cd(),
+    return MaterialApp(
+      home: Cd("Astroworld", 'cd_example.png'),
     );
+  }
+}
+
+// the Cd class
+class Cd extends StatelessWidget {
+  final String cdlist;
+  final String coverAlbum;
+  Cd(this.cdlist, this.coverAlbum);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xff222831),
+        body: Container(
+          child: Column(
+            children: [
+              Image.asset(
+                coverAlbum,
+              ),
+              FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text(
+                    cdlist,
+                    textAlign: TextAlign.justify,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontSize: 30),
+                  )),
+            ],
+          ),
+          width: MediaQuery.of(context).size.width * 0.2,
+          height: MediaQuery.of(context).size.width * 0.2,
+          margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: const Color(0xff393e46),
+              borderRadius: BorderRadius.circular(15)),
+        ));
   }
 }
