d6e01b33094ca2d85a129f94af8239c744bb363f
apoleon33 <simondumesny@gmail.com>
2022-01-17 20:04:45 +0100

    small improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'cd.dart';
 
 void main() {
   runApp(const Myapp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 // This is a basic Flutter widget test.
-//
+// ITS NOT WORKING
+// SOON
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
